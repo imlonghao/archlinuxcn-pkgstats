0ba86dc8d7bed21b88d7d660b0cbcb9dd12de525
imlonghao <git@imlonghao.com>
2022-12-28 09:50:13 +0800

    format: update quote

--- official.py
+++ official.py
@@ -9,7 +9,7 @@ from utils import *
 
 regexp = r'^(.*?) - - \[(.*?)\] "(.*?) (.*?) .*?" (\d+) \d+ ".*?" "(.*?)"$'
 SOURCE = "official"
-archs = ['aarch64', 'any', 'arm', 'armv6h', 'armv7h', 'i686', 'x86_64']
+archs = ["aarch64", "any", "arm", "armv6h", "armv7h", "i686", "x86_64"]
 
 
 def parser(line):
@@ -17,76 +17,83 @@ def parser(line):
     match = re.match(regexp, line)
     if not match:
         return result
-    result['remote_addr'] = match.group(1)
-    date = datetime.strptime(match.group(2), '%d/%b/%Y:%H:%M:%S %z')
-    result['time'] = int(datetime.timestamp(date))
-    result['method'] = match.group(3)
-    result['url'] = match.group(4)
-    result['status'] = int(match.group(5))
-    result['ua'] = match.group(6)
-    while '//' in result['url']:
-        result['url'] = result['url'].replace('//', '/')
-    path = result.get('url').split('/')
+    result["remote_addr"] = match.group(1)
+    date = datetime.strptime(match.group(2), "%d/%b/%Y:%H:%M:%S %z")
+    result["time"] = int(datetime.timestamp(date))
+    result["method"] = match.group(3)
+    result["url"] = match.group(4)
+    result["status"] = int(match.group(5))
+    result["ua"] = match.group(6)
+    while "//" in result["url"]:
+        result["url"] = result["url"].replace("//", "/")
+    path = result.get("url").split("/")
     if len(path) > 1 and path[1] not in archs:
         return result
-    if '.pkg.' in result['url']:
-        package = path[-1].split('.pkg.')[0].split('-')
+    if ".pkg." in result["url"]:
+        package = path[-1].split(".pkg.")[0].split("-")
         try:
-            result['package'] = {
-                'arch': path[2],
-                'name': '-'.join(package[:-3]),
-                'pkgver': package[-3],
-                'pkgrel': package[-2]
+            result["package"] = {
+                "arch": path[2],
+                "name": "-".join(package[:-3]),
+                "pkgver": package[-3],
+                "pkgrel": package[-2],
             }
         except:
             print(line)
             return result
-    elif result['url'].endswith('archlinuxcn.db'):
-        result['package'] = {
-            'arch': path[2],
-            'name': "archlinuxcn.db",
-            'pkgver': "0",
-            'pkgrel': "0"
+    elif result["url"].endswith("archlinuxcn.db"):
+        result["package"] = {
+            "arch": path[2],
+            "name": "archlinuxcn.db",
+            "pkgver": "0",
+            "pkgrel": "0",
         }
-    elif result['url'].endswith('archlinuxcn.files'):
-        result['package'] = {
-            'arch': path[2],
-            'name': "archlinuxcn.files",
-            'pkgver': "0",
-            'pkgrel': "0"
+    elif result["url"].endswith("archlinuxcn.files"):
+        result["package"] = {
+            "arch": path[2],
+            "name": "archlinuxcn.files",
+            "pkgver": "0",
+            "pkgrel": "0",
         }
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client = InfluxDBClient(DBHOST, DBPORT, DBUSER, DBPASS, DBNAME, True, True)
     result = []
+    ua_result = {}
     for line in sys.stdin:
         i = parser(line)
-        if i.get('package') and is_valid(i.get('remote_addr'), i):
-            geo = ip2geo(i.get('remote_addr'))
-            if '.' in i.get('remote_addr'):
+        if i.get("package") and is_valid(i.get("remote_addr"), i):
+            # new
+            if ua_result.get(i.get("ua")):
+                ua_result[i.get("ua")] += 1
+            else:
+                ua_result[i.get("ua")] = 1
+            # delete below
+            geo = ip2geo(i.get("remote_addr"))
+            if "." in i.get("remote_addr"):
                 address_type = 4
             else:
                 address_type = 6
-            result.append({
-                "measurement": "pkgstats",
-                "time": datetime.fromtimestamp(i.get('time'), timezone.utc),
-                "tags": {
-                    "source": SOURCE,
-                    'asn': geo.get('asn'),
-                    'country': geo.get('country'),
-                    'region': geo.get('region'),
-                    'city': geo.get('city'),
-                    'address_type': address_type,
-                    "arch": i.get('package').get('arch'),
-                    "name": i.get('package').get('name'),
-                    "pkgver": i.get('package').get('pkgver'),
-                    "pkgrel": i.get('package').get('pkgrel'),
-                    "ua": i.get('ua'),
-                },
-                "fields": {
-                    "value": 1
+            result.append(
+                {
+                    "measurement": "pkgstats",
+                    "time": datetime.fromtimestamp(i.get("time"), timezone.utc),
+                    "tags": {
+                        "source": SOURCE,
+                        "asn": geo.get("asn"),
+                        "country": geo.get("country"),
+                        "region": geo.get("region"),
+                        "city": geo.get("city"),
+                        "address_type": address_type,
+                        "arch": i.get("package").get("arch"),
+                        "name": i.get("package").get("name"),
+                        "pkgver": i.get("package").get("pkgver"),
+                        "pkgrel": i.get("package").get("pkgrel"),
+                        "ua": i.get("ua"),
+                    },
+                    "fields": {"value": 1},
                 }
-            })
+            )
     client.write_points(result, batch_size=5000)
